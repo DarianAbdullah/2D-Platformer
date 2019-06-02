817fe2dad4ec15fd9742a82dee5f4f8fc597a707
Shayan Mandegarian <smandegarian@yahoo.com>
2019-06-01 17:20:39 -0700

    added health

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -8,6 +8,10 @@ public class HeroController : MonoBehaviour
     public IHeroCommand Right;
     public IHeroCommand Left;
     public bool ground;
+    public bool cooling;
+    public int health = 10;
+    private float damageCoolDown = 1f;
+    private float counter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,17 @@ public class HeroController : MonoBehaviour
         {
             this.transform.rotation = new Quaternion(0, 0, 0, 0);
         }
+
+        if (cooling)
+        {
+            counter += Time.deltaTime;
+            if (counter > damageCoolDown)
+            {
+                cooling = false;
+                counter = 0;
+            }
+        }
+        Debug.Log(health);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -28,6 +43,15 @@ public class HeroController : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             ground = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && cooling == false)
+        {
+            health = health - 1;
+            cooling = true;
         }
     }
 
