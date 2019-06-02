bc167e4f33dc97e1163374f666c56a865a0f2528
DarianAbdullah <43215562+DarianAbdullah@users.noreply.github.com>
2019-06-01 18:31:27 -0700

    Created a main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int counter = 0;
+
+    private void Start()
+    {
+        counter = 1;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown("return") && counter == 1)
+        {
+            counter = 0;
+            PlayGame();
+        }
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
