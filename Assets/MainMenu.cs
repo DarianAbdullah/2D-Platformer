e1222594e03562891307d1c08eee52c4e7b64115
DarianAbdullah <43215562+DarianAbdullah@users.noreply.github.com>
2019-06-07 09:36:07 -0700

    Fixed menu, added controls

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -21,6 +21,7 @@ public class MainMenu : MonoBehaviour
     }
     public void PlayGame()
     {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex + 1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
