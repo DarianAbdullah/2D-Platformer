626dd9909227303cbad767611744d1c00b67b085
DarianAbdullah <43215562+DarianAbdullah@users.noreply.github.com>
2019-06-06 18:36:09 -0700

    Added menu and fixed level 1

---
+++ Assets/MainMenu1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu1 : MonoBehaviour
+{
+    public int counter = 0;
+
+    private void Start()
+    {
+        counter = 5;
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Submit") && counter > 0)
+        {
+            counter = 0;
+            PlayGame();
+        }
+    }
+    public void PlayGame()
+    {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
